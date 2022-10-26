602a011d3118f04d7ffd6c8516e2cb97518d900c
Samit Koyom <samitkoyom@gmail.com>
2022-10-27 03:45:01 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  // const Home({Key? key, required this.email}) : super(key: key);
+  // final String email;
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Welcome !'),
+      ),
+    );
+  }
+}
